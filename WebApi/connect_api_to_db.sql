fa59ea4f43f72d1c3ccf6c9c3693f086056b8590
darrel26 <dionisiusgunadi@outlook.com>
2024-01-09 16:25:35 +0700

    add sql command for web api

--- WebApi/connect_api_to_db.sql
+++ WebApi/connect_api_to_db.sql
@@ -1,3 +1,17 @@
 CREATE DATABASE kantor
 
 USE kantor
+
+CREATE TABLE employee (
+  id INT(11) NOT NULL AUTO_INCREMENT,
+  nama VARCHAR(15) NOT NULL,
+  jenis_kelamin ENUM('Laki-Laki', 'Perempuan') NOT NULL,
+  alamat VARCHAR(50) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+INSERT INTO data_diri (nama, jenis_kelamin, alamat) VALUES
+('Andi', 'Laki-Laki', 'Jakarta'),
+('Budi', 'Laki-Laki', 'Bandung'),
+('Cici', 'Perempuan', 'Semarang'),
+('Dedi', 'Laki-Laki', 'Surabaya');

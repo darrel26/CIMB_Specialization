590a543bff0bc31d1cc7ea13d995032c4a9a0643
darrel26 <dionisiusgunadi@outlook.com>
2024-01-11 11:19:36 +0700

    add additional file for TodoApp project

--- RESTFUL-WebAPI/TodoApp/Controllers/AuthManagementController.cs
+++ RESTFUL-WebAPI/TodoApp/Controllers/AuthManagementController.cs
@@ -128,7 +128,6 @@ namespace TodoApp.Controllers
                 Token = jwtToken,
                 Success = true,
                 RefreshToken = refreshToken.Token
-
             };
         }
 

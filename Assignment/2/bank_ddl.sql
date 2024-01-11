6e158e7de923975f9efc66bce6a7f10b1228a461
darrel26 <dionisiusgunadi@outlook.com>
2024-01-11 11:18:57 +0700

    add bank_ddl.sql script

---
+++ Assignment/2/bank_ddl.sql
@@ -0,0 +1,289 @@
+CREATE DATABASE bank;
+
+USE bank;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customers`
+--
+
+CREATE TABLE `customers` (
+  `customerNumber` int(11) NOT NULL,
+  `customerName` varchar(255) NOT NULL,
+  `contactLastName` varchar(50) NOT NULL,
+  `contactFirstName` varchar(50) NOT NULL,
+  `phone` varchar(20) DEFAULT NULL,
+  `addressLine1` varchar(100) DEFAULT NULL,
+  `addressLine2` varchar(100) DEFAULT NULL,
+  `city` varchar(50) DEFAULT NULL,
+  `state` varchar(2) DEFAULT NULL,
+  `postalCode` varchar(10) DEFAULT NULL,
+  `country` varchar(50) DEFAULT NULL,
+  `salesRepEmployeeNumber` int(11) DEFAULT NULL,
+  `creditLimit` decimal(10,2) DEFAULT NULL
+);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `employees`
+--
+
+CREATE TABLE `employees` (
+  `employeeNumber` int(11) NOT NULL,
+  `lastName` varchar(50) NOT NULL,
+  `firstName` varchar(50) NOT NULL,
+  `extension` varchar(10) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `officeCode` varchar(10) NOT NULL,
+  `reportsTo` int(11) DEFAULT NULL,
+  `jobTitle` varchar(50) NOT NULL
+);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `offices`
+--
+
+CREATE TABLE `offices` (
+  `officeCode` varchar(10) NOT NULL,
+  `city` varchar(50) NOT NULL,
+  `phone` varchar(20) DEFAULT NULL,
+  `addressLine1` varchar(100) DEFAULT NULL,
+  `addressLine2` varchar(100) DEFAULT NULL,
+  `state` varchar(2) DEFAULT NULL,
+  `country` varchar(50) DEFAULT NULL,
+  `postalCode` varchar(10) DEFAULT NULL,
+  `territory` varchar(10) DEFAULT NULL
+);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orderdetails`
+--
+
+CREATE TABLE `orderdetails` (
+  `orderLineNumber` int(11) NOT NULL,
+  `orderNumber` int(11) NOT NULL,
+  `productCode` varchar(50) NOT NULL,
+  `quantityOrdered` int(11) NOT NULL,
+  `priceEach` decimal(10,2) NOT NULL
+);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+CREATE TABLE `orders` (
+  `orderNumber` int(11) NOT NULL,
+  `orderDate` date NOT NULL,
+  `requiredDate` date DEFAULT NULL,
+  `shippedDate` date DEFAULT NULL,
+  `status` varchar(20) NOT NULL,
+  `comments` text DEFAULT NULL,
+  `customerNumber` int(11) NOT NULL
+);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `payments`
+--
+
+CREATE TABLE `payments` (
+  `paymentID` int(11) NOT NULL,
+  `customerNumber` int(11) NOT NULL,
+  `checkNumber` varchar(50) NOT NULL,
+  `paymentDate` date NOT NULL,
+  `amount` decimal(10,2) NOT NULL
+);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `productlines`
+--
+
+CREATE TABLE `productlines` (
+  `productLineID` int(11) NOT NULL,
+  `productLine` varchar(50) NOT NULL,
+  `textDescription` text DEFAULT NULL,
+  `htmlDescription` text DEFAULT NULL,
+  `image` blob DEFAULT NULL
+);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE `products` (
+  `productID` int(11) NOT NULL,
+  `productCode` varchar(50) NOT NULL,
+  `productName` varchar(100) NOT NULL,
+  `productLine` varchar(50) NOT NULL,
+  `productScale` varchar(20) DEFAULT NULL,
+  `productVendor` varchar(50) DEFAULT NULL,
+  `productDescription` text DEFAULT NULL,
+  `quantityInStock` int(11) DEFAULT NULL,
+  `buyPrice` decimal(10,2) DEFAULT NULL,
+  `MSRP` decimal(10,2) DEFAULT NULL
+);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `customers`
+--
+ALTER TABLE `customers`
+  ADD PRIMARY KEY (`customerNumber`),
+  ADD KEY `fk_salesRep` (`salesRepEmployeeNumber`);
+
+--
+-- Indexes for table `employees`
+--
+ALTER TABLE `employees`
+  ADD PRIMARY KEY (`employeeNumber`),
+  ADD KEY `fk_office` (`officeCode`),
+  ADD KEY `fk_reportsTo` (`reportsTo`);
+
+--
+-- Indexes for table `offices`
+--
+ALTER TABLE `offices`
+  ADD PRIMARY KEY (`officeCode`);
+
+--
+-- Indexes for table `orderdetails`
+--
+ALTER TABLE `orderdetails`
+  ADD PRIMARY KEY (`orderLineNumber`),
+  ADD KEY `fk_order` (`orderNumber`),
+  ADD KEY `fk_product` (`productCode`);
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`orderNumber`),
+  ADD KEY `fk_customer_order` (`customerNumber`);
+
+--
+-- Indexes for table `payments`
+--
+ALTER TABLE `payments`
+  ADD PRIMARY KEY (`paymentID`),
+  ADD UNIQUE KEY `customerNumber` (`customerNumber`,`checkNumber`);
+
+--
+-- Indexes for table `productlines`
+--
+ALTER TABLE `productlines`
+  ADD PRIMARY KEY (`productLineID`),
+  ADD UNIQUE KEY `productLine` (`productLine`);
+
+--
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`productID`),
+  ADD UNIQUE KEY `productCode` (`productCode`),
+  ADD KEY `fk_productLine` (`productLine`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `customers`
+--
+ALTER TABLE `customers`
+  MODIFY `customerNumber` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `employees`
+--
+ALTER TABLE `employees`
+  MODIFY `employeeNumber` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `orderdetails`
+--
+ALTER TABLE `orderdetails`
+  MODIFY `orderLineNumber` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `orderNumber` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `payments`
+--
+ALTER TABLE `payments`
+  MODIFY `paymentID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `productlines`
+--
+ALTER TABLE `productlines`
+  MODIFY `productLineID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `products`
+--
+ALTER TABLE `products`
+  MODIFY `productID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `customers`
+--
+ALTER TABLE `customers`
+  ADD CONSTRAINT `fk_salesRep` FOREIGN KEY (`salesRepEmployeeNumber`) REFERENCES `employees` (`employeeNumber`);
+
+--
+-- Constraints for table `employees`
+--
+ALTER TABLE `employees`
+  ADD CONSTRAINT `fk_office` FOREIGN KEY (`officeCode`) REFERENCES `offices` (`officeCode`) ON DELETE CASCADE,
+  ADD CONSTRAINT `fk_reportsTo` FOREIGN KEY (`reportsTo`) REFERENCES `employees` (`employeeNumber`);
+
+--
+-- Constraints for table `orderdetails`
+--
+ALTER TABLE `orderdetails`
+  ADD CONSTRAINT `fk_order` FOREIGN KEY (`orderNumber`) REFERENCES `orders` (`orderNumber`) ON DELETE CASCADE,
+  ADD CONSTRAINT `fk_product` FOREIGN KEY (`productCode`) REFERENCES `products` (`productCode`);
+
+--
+-- Constraints for table `orders`
+--
+ALTER TABLE `orders`
+  ADD CONSTRAINT `fk_customer_order` FOREIGN KEY (`customerNumber`) REFERENCES `customers` (`customerNumber`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `payments`
+--
+ALTER TABLE `payments`
+  ADD CONSTRAINT `fk_customer` FOREIGN KEY (`customerNumber`) REFERENCES `customers` (`customerNumber`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `products`
+--
+ALTER TABLE `products`
+  ADD CONSTRAINT `fk_productLine` FOREIGN KEY (`productLine`) REFERENCES `productlines` (`productLine`) ON DELETE CASCADE;
+ 
+COMMIT;

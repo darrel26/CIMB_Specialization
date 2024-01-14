60371da490116bf74a08bd475281e979e250342d
darrel26 <dionisiusgunadi@outlook.com>
2024-01-14 09:37:18 +0700

    Todo Mock Test

--- RESTFUL-WebAPI/Todo_UnitTest_Mock/TodoTest.cs
+++ RESTFUL-WebAPI/Todo_UnitTest_Mock/TodoTest.cs
@@ -4,6 +4,7 @@ using TodoApp.Services;
 using TodoApp.Models;
 using Xunit;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 
 namespace Todo_UnitTest_Mock
 {
@@ -20,12 +21,52 @@ namespace Todo_UnitTest_Mock
             _mock = new Mock<ITodoService>();
             _controller = new TodoController(_mock.Object);
         }
-        #endregion  
-
-        public IActionResult GetItemById()
+        #endregion
+        [Fact]
+        public async void GetItemById()
         {
-            var item = _controller.GetItemById(1);
-            return View(item);
+            _mock.Setup(service => service.GetItemById(1)).ReturnsAsync(new ItemData() { 
+                Id = 1,
+                Title = "Wongko",
+                Description = "Jember",
+                Done = true
+            });
+
+            var result = await _controller.GetItemById(1);
+            _mock.Verify(r => r.GetItemById(1));
+            // Check the response is OK (StatusCode 200)
+            Assert.IsType<OkObjectResult>(result);
+
+            // Check value type inside OK result 
+            var item = result as OkObjectResult;
+            Assert.IsType<ItemData>(item.Value);
+
+            // Check returned item value 
+            var data = item.Value as ItemData;
+            Assert.Equal("Wongko", data.Title);
+        }
+
+        // Unit Test without Mock (Moq)
+        [Fact]
+        public async void GetItems()
+        {
+            _mock.Setup(service => service.GetItems()).ReturnsAsync(new List<ItemData>() { 
+                new ItemData(), 
+                new ItemData() 
+            });
+
+            var result = await _controller.GetItems();
+            _mock.Verify(r => r.GetItems());
+
+            Assert.IsType<OkObjectResult>(result);
+            
+            var item = result as OkObjectResult;
+
+            Assert.IsType<List<ItemData>>(item.Value);
+
+            var itemData = item.Value as List<ItemData>;
+
+            Assert.Equal(2, itemData.Count);
         }
     }
 }
